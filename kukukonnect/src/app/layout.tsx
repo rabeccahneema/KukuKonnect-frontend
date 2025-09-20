ab273b5964f77d206d15cf538b623c77853f3714
Linn Amondi <linnamondi@gmail.com>
2025-09-20 12:52:39 +0300

    added layout.tsx file

--- kukukonnect/src/app/layout.tsx
+++ kukukonnect/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Sidebar from "./shared-components/Sidebar/Agrovet-Sidebar";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,9 +26,9 @@ export default function RootLayout({
     <html lang="en">
      
       <body
-    
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+         <Sidebar/>
         {children}
       </body>
     </html>
